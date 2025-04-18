46cdb4dbe6f668b41d8d2205a9556f353a222330
DandanLeinad <danieldias6788@gmail.com>
2025-04-18 16:56:09 -0300

    feat: simplificar a função get_java_worlds_path removendo tratamento de erros e ajustando caminhos

--- src/backup/detect_java.py
+++ src/backup/detect_java.py
@@ -5,18 +5,12 @@ import platform
 def get_java_worlds_path():
     system = platform.system()
     if system == "Windows":
-        path = os.path.expandvars(r"%APPDATA%\.minecraft\saves")
-    elif system == "Darwin":
-        path = os.path.expanduser(
+        return os.path.expanduser("~\\AppData\\Roaming\\.minecraft\\saves")
+    elif system == "Darwin":  # macOS
+        return os.path.expanduser(
             "~/Library/Application Support/minecraft/saves"
         )
     elif system == "Linux":
-        path = os.path.expanduser("~/.minecraft/saves")
+        return os.path.expanduser("~/.minecraft/saves")
     else:
-        raise OSError("Sistema operacional não suportado.")
-
-    if not os.path.exists(path):
-        raise FileNotFoundError(
-            f"❌ Minecraft Java Edition não encontrado ou nenhum mundo salvo em: {path}" # NOQA
-        )
-    return path
+        return None
