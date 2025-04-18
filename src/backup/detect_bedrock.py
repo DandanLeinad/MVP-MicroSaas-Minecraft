6a21e07db62fae98f86bf1e247de1f5e98f6d022
DandanLeinad <danieldias6788@gmail.com>
2025-04-18 16:57:53 -0300

    feat: refatorar função get_bedrock_worlds_path para simplificar o retorno do caminho e remover exceções

--- src/backup/detect_bedrock.py
+++ src/backup/detect_bedrock.py
@@ -3,15 +3,10 @@ import platform
 
 
 def get_bedrock_worlds_path():
-    if platform.system() == "Windows":
-        path = os.path.expandvars(
-            r"%LOCALAPPDATA%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds" # NOQA
+    system = platform.system()
+    if system == "Windows":
+        return os.path.expanduser(
+            "~\\AppData\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\minecraftWorlds"  # NOQA
         )
     else:
-        raise OSError("Bedrock Edition é suportada apenas no Windows.")
-
-    if not os.path.exists(path):
-        raise FileNotFoundError(
-            f"❌ Minecraft Bedrock Edition não encontrado ou nenhum mundo salvo em: {path}" # NOQA
-        )
-    return path
+        return None  # Bedrock só disponível no Windows (oficialmente)
